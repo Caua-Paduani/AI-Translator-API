4473397e928ec08a4918f6750249f5e2d8d68fc6
Cauã Paduani <cauapaduani@gmail.com>
2025-08-07 09:53:20 -0300

    update main

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-from openai import OpenAI
-from fastapi import FastAPI, Request
-from dotenv import load_dotenv
+
+from fastapi import FastAPI, HTTPException
+import requests
+import re
+import time
 import os
-from pydantic import BaseModel
 
-load_dotenv()
+cache = {}
+CACHE_DURATION = 3600 # 1 hour
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 app = FastAPI()
 
-client = OpenAI(
-  api_key=OPENAI_API_KEY
-)
+def validate_cep(cep: str) -> str:
+    cep_limpo = re.sub(r'\D', '', cep)
+    if len(cep_limpo) != 8:
+        raise HTTPException(status_code=400, detail="CEP deve ter 8 dígitos")
+    return cep_limpo
 
+        
 
-response = client.responses.create(
-  model="gpt-4o-mini",
-  input="write a haiku about ai",
-  store=True,
-)
-
-
+@app.get("/buscar-cep")
+async def buscar_cep(cep: str):
+    cep_limpo = validate_cep(cep)
+    agora = time.time()
+    #verifica se o cep está no cache
+    if cep_limpo in cache and agora - cache[cep_limpo]['timestamp'] < CACHE_DURATION:
+        return cache[cep_limpo]['data']
+    #se não estiver, busca no viacep
+    url = f"https://viacep.com.br/ws/{cep_limpo}/json/"
+    response = requests.get(url)
+    if response.status_code != 200:
+        return {"error": "CEP não encontrado"}
+    dados = response.json()
+    
+    return dados 
